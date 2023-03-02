4997e7fe74f8d96008e9e32e3c0c59e4cb7e5685
Rubial Alom <rubelalam75@gmail.com>
2023-03-02 15:28:42 +0530

    second commit

--- src/client.js
+++ src/client.js
@@ -3,13 +3,13 @@ import imageUrlBuilder from "@sanity/image-url";
 
 const projectId = import.meta.env.VITE_REACT_SANITY_PROJECT_ID;
 const dataset = import.meta.env.VITE_REACT_SANITY_DATASET; // "production"
-const apiVersion = import.meta.env.VITE_REACT_SANITY_API_VERSION; // "2022-11-16"
+// const apiVersion = import.meta.env.VITE_REACT_SANITY_API_VERSION; // "2022-11-16"
 const token = import.meta.env.VITE_REACT_SANITY_TOKEN;
 
 export const client = createClient({
   projectId,
   dataset,
-  apiVersion,
+  apiVersion: "2021-08-31",
   useCdn: false,
   token,
   ignoreBrowserTokenWarning: true,
